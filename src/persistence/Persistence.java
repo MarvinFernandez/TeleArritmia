4d7628efe8b0e495b715093f964b877cc8537e13
Daniel Marchan <danielmarchan3@gmail.com>
2019-10-25 15:31:14 +0200

    modificacion persistencia

--- src/persistence/Persistence.java
+++ src/persistence/Persistence.java
@@ -56,7 +56,7 @@ public class Persistence {
 
 
       
-    /*Le paso un path y me tiene que guardar esa poblacion ahi*/
+    /*Le paso un path y me tiene que guardar ese paciente ahi*/
     public void guardar(File f,Patient patient){
         FileOutputStream pat = null;     
         ObjectOutputStream pati = null;    
@@ -89,44 +89,6 @@ public class Persistence {
             }
         }
     }
-    
-    
-    
-    
-     /*El file se lee por pantalla es muy sencillo 
-        Esto lo vamos a meter dentro del boton para cargar una poblacion 
-    
-            System.out.println("Introduzca el path del archivo que contiene la poblacion:");
-            
-            String nom=leer();
-            f = new File(nom);
-            
-            if(!f.exists()){
-                    System.out.println("Archivo inexistente");break;
-            }
-            
-            else        
-                    try{  
-                        p=per.cargar(f);
-                        System.out.println(p);break;
-                    }
-                    catch(ClassNotFoundException ex){
-                        System.out.println("error en la lectura intente de nuevo");
-                        break;
-                    }       
-            */
-    
-    
-    /*Esto para guardar
-    
-    System.out.println("Escriba el nuevo path de su archivo:");
-                String nuevo=leer();   
-                f = new File(nuevo);
-                per.guardar(f, p);
-                System.out.println("Creado");
-                System.out.println("\n");
-    
-    */
     
     
 }
